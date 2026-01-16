06d74d23c18e80de4ac1ccc96fb086fe8d8151fd
wisnu-10 <wisnubintoro1997@gmail.com>
2026-01-16 11:07:58 +0700

    fixing scroll to top

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,10 +3,12 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ScrollToTop from "../components/ScrollToTop";
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
+      <ScrollToTop />
       <Navbar />
       <main className="grow pt-20 flex flex-col">
         <Outlet />
